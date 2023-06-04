590ae43763e6c13438aa26f78b49fbd64d31588f
OlgaVoronkina <voronkina.olga.s@gmail.com>
2023-06-04 19:35:23 +0300

    исправлен SelenideTest1. Добавлена проверка многострочного текста.

--- src/test/java/github/SelenideTest1.java
+++ src/test/java/github/SelenideTest1.java
@@ -1,11 +1,11 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import demoqaHW.BaseTests;
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest1 extends BaseTests {
@@ -16,8 +16,17 @@ public class SelenideTest1 extends BaseTests {
         open("https://github.com/selenide/selenide"); // Откройте страницу Selenide в Github
         $("#wiki-tab").click();                                     // - Перейдите в раздел Wiki проекта
         $("#wiki-pages-filter").val("SoftAssertions");              // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $("div.wiki-rightbar a[href*='SoftAssertions']").shouldHave(Condition.text("SoftAssertions")).click();
-        $("a[id*=junit5]").closest("h4").shouldHave(Condition.text("JUnit5"));  // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-        $$(By.xpath("//a[contains(@id, 'junit5')]/ancestor::h4/following-sibling::div[1]//span")).findBy(Condition.text("Test")).shouldBe(Condition.visible);
+        $("div.wiki-rightbar a[href*='SoftAssertions']").shouldHave(text("SoftAssertions")).click();
+        $("a[id*=junit5]").closest("h4").shouldHave(text("JUnit5"));  // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $("a[id*=junit5]").closest("h4").sibling(0).shouldHave(text
+                ("@Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));;
     }
 }
